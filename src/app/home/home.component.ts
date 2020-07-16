28f9092e505bc274eab1a321a7447679c655778c
Fichtner21 <ernest.fichtner@gmail.com>
2020-07-16 12:14:00 +0200

    Added to home working todo list integration with cloud firestore

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,68 @@
+/**
+ * Komponent wyświetlający listę zadań pobranych z bazy danych Firebase.
+ */
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, DocumentChangeAction } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Todo } from './todo.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public todos$: Observable<any>;
-  public newTodo = '';
+  /**
+   * Statyczna stałą przechowująca nazwę kolekcji w bazie danych Firebase.
+   */
+  private static readonly COLLECTION_KEY = 'todos';
 
-  constructor(private firestore: AngularFirestore) { }
+  /**
+   * Strumień przechowujący aktualny model danych pobrany z Firestore.
+   */
+  public todos$: Observable<Todo[]>;
 
+  /**
+   * Wstrzyknięty w konstruktorze serwis `AngularFirestore` służy do komunikacji z bazą danych Firebase.
+   */
+  constructor(private firestore: AngularFirestore) {}
+
+  /**
+   * Podczas inicjowania komponentu wartość pola `todos$` ustawiana jest na strumień zmian wartości wskazanej kolekcji.
+   * Dowolna zmiana w modelu danych w bazie danych powoduje wyemitowanie nowej wartości.
+   * Przekazany do metody `valueChanges` obiekt pozwala na zdefiniowanie pola z bazy danych służącego do identyfikacji elementów kolekcji.
+   */
   ngOnInit(): void {
-    //this.todos$ = this.firestore.collection('todos').valueChanges()
-
-    // this.firestore.collection('todos').snapshotChanges().pipe(
-    //   map((snapshot: DocumentChangeAction<any>[]) => snapshot.map(action => action.payload.doc.data()))
-    // );
-    // .subscribe((changes: DocumentChangeAction<any>[]) => {
-
-    // });
+    this.todos$ = this.firestore.collection<Todo>(HomeComponent.COLLECTION_KEY).valueChanges({ idField: 'id' });
   }
 
-  public addTodo(){
-    this.firestore.collection('todos').add({
-      text: this.newTodo,
+  /**
+   * Metoda dodawania dokumentów do kolekcji w bazie danych.
+   */
+  public addTodo(text: string) {
+    this.firestore.collection(HomeComponent.COLLECTION_KEY).add({
+      text,
       done: false,
-    })
+    });
+  }
+
+  /**
+   * Metoda służąca do aktualizacji pól dokumentu ze wskazanej kolekcji.
+   */
+  public setDone(todo: Todo, done: boolean) {
+    this.firestore
+      .collection(HomeComponent.COLLECTION_KEY)
+      .doc(todo.id)
+      .update({ done });
+  }
+
+  /**
+   * Metoda służąca do kasowania dokumentów o przekazanym ID z kolekcji.
+   */
+  public deleteTodo(todo: Todo) {
+    this.firestore
+      .collection(HomeComponent.COLLECTION_KEY)
+      .doc(todo.id)
+      .delete();
   }
 }
